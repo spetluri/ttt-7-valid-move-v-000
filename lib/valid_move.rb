f5b2cf468b91846e2267ea36ef2019c737ebb6da
Sai Petluri <spetluri.1@gmail.com>
2021-01-21 15:35:06 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,5 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-  
-  position = position.to_i
 
   if position.between?(0,8) && position_taken?(board,position) == false
     true
